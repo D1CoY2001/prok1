788203744acd10c290a8eeb3848f6bbd6512dc9f
Beto test <rasskazowruslan@gmail.com>
2026-08-25 16:20:06 +0300

    Implement mobile account bonus calculation

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,11 +2,18 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        int ticketPrice = 13_676;
-        int rublesPerMile = 20;
+        int initialBalance = 100;
+        int replenishment = 1_100;
 
-        int bonusMiles = ticketPrice / rublesPerMile;
+        int bonus = 0;
 
-        System.out.println(bonusMiles);
+        if (replenishment > 1_000) {
+            bonus = replenishment / 100;
+        }
+
+        int totalBalance = initialBalance + replenishment + bonus;
+
+        System.out.println("Итоговый счёт: " + totalBalance);
+        System.out.println("Бонусные рубли: " + bonus);
     }
 }
